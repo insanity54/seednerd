d19144dfc72ebedc019087d541779dc7ef40b9e1
Christopher Joule Xenon <chris@grimtech.net>
2025-05-29 12:37:07 -0800

    init

---
+++ index.js
@@ -0,0 +1,26 @@
+// index.js
+
+const concurrently = require('concurrently');
+const path = require('path')
+
+const { result } = concurrently(
+    [
+        { command: 'npm:enter', name: 'enter' },
+        { command: 'npm:luanti', name: 'luanti' }
+    ],
+    {
+        prefix: 'name',
+        killOthers: ['failure', 'success'],
+        cwd: path.resolve(__dirname, 'scripts'),
+    },
+);
+
+
+result.then(
+    () => console.log('✅ All processes exited successfully.'),
+    (err) => {
+        console.error('❌ One or more processes failed:');
+        console.error(err);
+        process.exit(1); // Optional: force a non-zero exit
+    }
+);
